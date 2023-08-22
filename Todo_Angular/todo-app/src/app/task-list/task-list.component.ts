6248d8cb4f96e1d80c99a859a8e5f6b5a63b7d5d
Satyamjha24 <pananrocks18@gmail.com>
2023-08-22 13:50:23 +0530

    angular

---
+++ Todo_Angular/todo-app/src/app/task-list/task-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Task } from '../task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent {
+  tasks: Task[] = [];
+}
